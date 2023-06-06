4759b8c8f0cd7c4a42881d0ac6b1729240a797b8
Diego Garcia <dagadev@proton.me>
2023-06-06 13:34:03 -0500

    Song square updted

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import { useState } from 'react'
 
 function App() {
-  const [song, setSong] = useState('')
-  const [showSong, setShowSong] = useState(false)
-
-  const changeSong = (e) => {
-    setSong(e.target.value)
-  }
-
-  const SampleSong = {
+  const [songLink, setSongLink] = useState('')
+  const [song, setSong] = useState({
     title: 'Sample Song',
     artist: 'Sample Artist',
     album: 'Sample Album',
-    cover: 'https://i.scdn.co/image/ab67616d0000b273e4b5b3b2b2b2b2b2b2b2b2b2',
-    link: 'https://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V?si=2e8c7b0e0b5c4b9b'
+    cover: 'https://cdn.dribbble.com/users/12084/screenshots/1867046/vinyl_mockup_soundsortium_1x.jpg',
+    link: 'https://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V?si=2e8c7b0e0b5c4b9b'})
+
+  const changeSong = (e) => {
+    setSongLink(e.target.value)
   }
 
   const searchHandler = async () => {
-    setShowSong(true)
-    const url = song.split('/')[4]
+    const url = songLink.split('/')[4]
     console.log(url)
     const response = await fetch(`https://spotify-downloader1.p.rapidapi.com/download/${url}`, {
       "method": "GET",
@@ -28,7 +24,16 @@ function App() {
       }
     })
     const data = await response.json()
-    setSong(data ?? SampleSong)
+
+    setSong({
+      title: data.metadata.title,
+      artist: data.metadata.title,
+      album: data.metadata.album,
+      cover: data.metadata.cover,
+      link: data.link
+    })
+
+    console.log(song)
   }
 
   return (
@@ -45,16 +50,21 @@ function App() {
       
       <div className='mt-10'>
         <div className="flex flex-col justify-center items-center">
-          <img src={song.metadata.cover ?? '#'} alt={song.metadata.title ?? '#'} className="w-40 h-40 rounded-md"/>
-          <h2 className="text-2xl font-bold pt-2">{song.metadata.title ?? '#'}</h2>
-          <h3 className="text-xl font-bold pt-2">{song.metadata.album ?? '#'}</h3>
-          <h4 className="text-xl font-bold pt-2">{song.metadata.artists ?? '#'}</h4> 
-          <a href={song.metadata.link ?? '#'} target="_blank" rel="noreferrer" className="btn btn-primary rounded-lg max-w-md mt-6 mx-auto w-full">Download</a>
+          <img src={song.cover} alt={song.title} className="w-40 h-40 rounded-md"/>
+          <h2 className="text-2xl font-bold pt-2">{song.title}</h2>
+          <h3 className="text-xl font-bold pt-2">{song.album}</h3>
+          <h4 className="text-xl font-bold pt-2">{song.artist}</h4> 
+          <a href={song.link} target="_blank" rel="noreferrer" className="btn btn-primary rounded-lg max-w-md mt-6 mx-auto w-full">Download</a>
         </div>
       </div>
-      
+
+      <footer className='p-10'>
+        <p className="text-center text-gray-500 text-sm mt-10">Made with <i className="fas fa-heart text-red-500"></i> by <a href="" className="text-blue-500">Diego Garcia</a></p>
+      </footer>
     </main>
   )
+
+
 }
 
 export default App
